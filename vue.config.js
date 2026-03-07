020a64170082631c9c5228507f6cad5228abdd87
marvin <929394361@qq.com>
2026-03-07 09:52:58 +0800

    --fix: 修改打包文件名

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
-  outputDir: 'dist',
+  outputDir: 'docs',
   assetsDir: 'static',
   productionSourceMap: false,
   devServer: {
